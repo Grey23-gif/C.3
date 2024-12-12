877cf0020cc1ca46a6b3731b2a395b9125b52a2c
Grey <duong230426@gmail.com>
2024-12-12 19:43:20 +0700

    Update 3.8.cpp

--- 3.8.cpp
+++ 3.8.cpp
@@ -1,20 +1,15 @@
 #include <stdio.h>
-#include <string.h>
 
 int main() 
 {
-    int number, sum =0;
+    int number, lap ;
     printf("Nhap mot so nguyen: ");
     scanf("%d", &number);
     
     char so[44];
     sprintf(so, "%d", number);
     
-    printf("So dao nguoc cua %d la: ", number);
-    for(int i=strlen(so);i>-1 ;i--)
-    {
-        printf("%c", so[i]);
-    }
- 
+    lap = (number % 10) * 1000 + ((number / 10) % 10) * 100 + ((number / 100) % 10) * 10 + (number / 1000);
+    printf("Dao nguoc cua %d la: %d",number,lap);
     return 0;
 }
